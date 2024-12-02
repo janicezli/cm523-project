0370a069b151e768ee5439d22e36b6e8d16e7870
janicezli <janicel@bu.edu>
2024-12-02 05:55:48 -0500

    commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,6 +28,46 @@ var markersData = [
         title: "Caffe Vittoria",
         popupContent: "<p><b>Caffe Vittoria</b></p>"
     }
+    {
+        position: [42.365187, -71.055601],  
+          title: "Bova's Bakery",
+          popupContent: "<p><b>Bova's Bakery</b></p>"
+      },
+    {
+        position: [42.363249, -71.054753],  
+          title: "Modern Pastry Shop",
+          popupContent: "<p><b>Modern Pastry Shop</b></p>"
+    },
+      {
+        position: [42.342211, -71.080107],  
+          title: "Colette Bakery",
+          popupContent: "<p><b>Colette Bakery</b></p>"
+      },
+      {
+        position: [42.356171, -71.053307],  
+          title: "Kane's Donuts",
+          popupContent: "<p><b>Kane's Donuts</b></p>"
+      },
+    {
+        position: [42.350196, -71.079404],  
+          title: "Levain Bakery",
+          popupContent: "<p><b>Levain Bakery</b></p>"
+      },
+    {
+        position: [42.3514443, -71.0766509],  
+          title: "Jonquils Cafe and Bakery",
+          popupContent: "<p><b>Jonquils Cafe and Bakery</b></p>"
+      },
+    {
+        position: [42.3505169, -71.0599519],  
+          title: "Sweet Waffles + Boba",
+          popupContent: "<p><b>Sweet Waffles + Boba</b></p>"
+      },
+    {
+        position: [42.34973907470703, -71.08280944824219],  
+          title: "Amorino Gelato",
+          popupContent: "<p><b>Amorino Gelato</b></p>"
+      },
 ];
 
 markersData.forEach(function(markerData) {
