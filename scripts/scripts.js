613f55dd90686c5b5ee23777b3c61a180e273c3b
janicezli <janicel@bu.edu>
2024-12-04 21:26:47 -0500

    commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,15 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
-var map = L.map('map', {
+const map = L.map('map', {
     center: [42.3601, -71.0589],
     zoom: 10,
- 
-var tiles = new L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  });
+const tiles = new L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     minZoom: 15  
 }).addTo(map);
 
-<!---var marker = L.marker([42.3641884, -71.0542414], { 
-   <!--- draggable: true,
-   <!--- title: "Mike's Pastry",
-   <!--- opacity: 0.75
-<!---});
 
-<!---marker.addTo(map).bindPopup("<p><b>Mike's Pastry</b></p>");
+marker.addTo(map).bindPopup("<p><b>Mike's Pastry</b></p>");
 
 const markersData = [
     {
@@ -66,7 +61,7 @@ const markersData = [
         position: [42.34973907470703, -71.08280944824219],  
           title: "Amorino Gelato",
           popupContent: "<p><b>Amorino Gelato</b></p>"
-      };
+      }
 ];
 
 markersData.forEach(function(markerData) {
